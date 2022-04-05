dfb8a1fbd89a34fe67ab84e67cc1d58b25c5653a
Leonardo <josue.aguilar1995@gmail.com>
2022-04-04 20:13:28 -0500

    created concatinating arrays and pulled random characters

--- Develop/script.js
+++ Develop/script.js
@@ -6,9 +6,15 @@
 var startUp = function () {
   askPasswordLength();
   askPasswordCriteria();
-};
 
-// Uses responses to define how to randomly choose
+  // Generate random password
+  for (var i = 0; i < passwordLength; i++) {
+    password =
+      password + criteriaArray[randomNumber(0, criteriaArray.length - 1)];
+  }
+
+  console.log(password);
+};
 
 // Creating a function that lets us choose randomly based on two variables
 var randomNumber = function (min, max) {
@@ -19,7 +25,6 @@ var randomNumber = function (min, max) {
 
 // Asks for how many characters we need the password to be
 var askPasswordLength = function () {
-  var passwordLength = "";
   // Loop to check if length is eligible
   while (
     isNaN(passwordLength) ||
@@ -36,6 +41,8 @@ var askPasswordLength = function () {
   console.log(
     "You have requested a password length of " + passwordLength + " characters."
   );
+
+  return passwordLength;
 };
 
 // Asks for which criteria they want to use in a row
@@ -43,11 +50,35 @@ var askPasswordCriteria = function () {
   var lowercaseConfirm = window.confirm(
     "Would you like to use LOWERCASE letters in your password?"
   );
+
+  // Add lowercase if confirmed
+  if (lowercaseConfirm) {
+    criteriaArray = lowercase;
+  }
+
   var uppercaseConfirm = window.confirm(
     "Would you like to use UPPERCASE letters in your password?"
   );
 
-  if (1 > lowercaseConfirm + uppercaseConfirm) {
+  // Add uppercase if confirmed
+  if (uppercaseConfirm && !lowercase) {
+    criteriaArray = uppercase;
+  } else if (uppercaseConfirm) {
+    criteriaArray = criteriaArray.concat(uppercase);
+  }
+
+  var numbersConfirm = window.confirm(
+    "Would you like to use NUMBERS in your password?"
+  );
+
+  // Add numbers if confirm
+  if (numbersConfirm && !lowercase && !uppercase) {
+    criteriaArray = numbers;
+  } else if (numbersConfirm) {
+    criteriaArray = criteriaArray.concat(numbers);
+  }
+
+  if (1 > lowercaseConfirm + uppercaseConfirm + numbersConfirm) {
     window.alert(
       "Please choose at lease one set of criteria for your password"
     );
@@ -58,10 +89,68 @@ var askPasswordCriteria = function () {
 };
 
 // Password criteria arrays declarations
-var passwordCriteria = Array();
+var criteriaArray = [];
 
-passwordCriteria[0] = Array("a", "b", "c");
-passwordCriteria[1] = Array("A", "B", "C");
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// passwordLength declaration
+var passwordLength = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
